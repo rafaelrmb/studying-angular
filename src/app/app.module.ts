b1d029d50679499641e35977a28c68342a56eb99
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-17 10:52:01 -0300

    add success alert component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './components/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './components/success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
