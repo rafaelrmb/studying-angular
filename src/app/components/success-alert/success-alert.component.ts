ff90e00be59a6629ad7ae8db47c9309d68ec7d06
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-17 11:06:14 -0300

    add basic string interpolation

--- src/app/components/success-alert/success-alert.component.ts
+++ src/app/components/success-alert/success-alert.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
-  styleUrls: ['./success-alert.component.css']
+  styleUrls: ['./success-alert.component.css'],
 })
 export class SuccessAlertComponent {
-
+  operationId: number = 54678;
 }
