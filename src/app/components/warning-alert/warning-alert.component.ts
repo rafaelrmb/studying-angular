4a9d4b848e74e237dc1130c81ee64ce7cba47d65
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-17 11:23:53 -0300

    add button function with property binding

--- src/app/components/warning-alert/warning-alert.component.ts
+++ src/app/components/warning-alert/warning-alert.component.ts
@@ -5,4 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './warning-alert.component.html',
   styleUrls: ['./warning-alert.component.css'],
 })
-export class WarningAlertComponent {}
+export class WarningAlertComponent {
+  confirmButton: boolean = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.confirmButton = true;
+    }, 2000);
+  }
+  confirm() {
+    confirm('Are you sure?');
+  }
+}
