fe7ba0f17b5e4ddf65b827706386cb44661668a1
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-18 11:35:51 -0300

    add cockpit and server element components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './components/warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './components/success-alert/success-alert.component';
+import { CockpitComponent } from './components/cockpit/cockpit.component';
+import { ServerElementComponent } from './components/server-element/server-element.component';
 
 @NgModule({
-  declarations: [AppComponent, WarningAlertComponent, SuccessAlertComponent],
+  declarations: [AppComponent, WarningAlertComponent, SuccessAlertComponent, CockpitComponent, ServerElementComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
