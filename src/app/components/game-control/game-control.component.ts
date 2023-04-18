42f8de00b69e2662d093bd7a9de19313d15093a5
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-18 15:34:56 -0300

    add data binding and functions to component

--- src/app/components/game-control/game-control.component.ts
+++ src/app/components/game-control/game-control.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent {
+  @Output('refreshNumber') gameStarted = new EventEmitter<number>();
+  @Output() gameStopped = new EventEmitter<number>();
 
+  ref: any;
+  num: number = 1;
+
+  startGame() {
+    if (this.ref) {
+      clearInterval(this.ref);
+    }
+
+    this.ref = setInterval(() => {
+      this.gameStarted.emit(this.num++);
+    }, 1000);
+  }
+
+  stopGame() {
+    if (this.ref) {
+      clearInterval(this.ref);
+      this.ref = null;
+    }
+  }
 }
