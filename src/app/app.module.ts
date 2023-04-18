6998917b74669b8f4262b5dc5137552953ca7549
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-18 14:28:24 -0300

    add assignment components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import { WarningAlertComponent } from './components/warning-alert/warning-alert.
 import { SuccessAlertComponent } from './components/success-alert/success-alert.component';
 import { CockpitComponent } from './components/cockpit/cockpit.component';
 import { ServerElementComponent } from './components/server-element/server-element.component';
+import { GameControlComponent } from './components/game-control/game-control.component';
+import { OddComponent } from './components/odd/odd.component';
+import { EvenComponent } from './components/even/even.component';
 
 @NgModule({
-  declarations: [AppComponent, WarningAlertComponent, SuccessAlertComponent, CockpitComponent, ServerElementComponent],
+  declarations: [AppComponent, WarningAlertComponent, SuccessAlertComponent, CockpitComponent, ServerElementComponent, GameControlComponent, OddComponent, EvenComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
