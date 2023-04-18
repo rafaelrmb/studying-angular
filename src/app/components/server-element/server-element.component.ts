a9c1b7bb889d6e946fd118ecbe6005dd56efe157
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-18 12:12:21 -0300

    add data binding to element object

--- src/app/components/server-element/server-element.component.ts
+++ src/app/components/server-element/server-element.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
 })
 export class ServerElementComponent {
-
+  @Input('element')
+  element!: { type: string; name: string; content: string };
 }
