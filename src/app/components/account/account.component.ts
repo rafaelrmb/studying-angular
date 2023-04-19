77b2d703bcb4c960eadb084578fe897861434d79
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-19 14:29:40 -0300

    add functionality change account status

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { LoggingService } from 'src/app/services/logging.service';
 
 @Component({
   selector: 'app-account',
@@ -7,4 +8,12 @@ import { Component, Input } from '@angular/core';
 })
 export class AccountComponent {
   @Input('newAccount') newAccount!: { name: string; status: string };
+
+  constructor(private logger: LoggingService) {}
+
+  changeAccountStatus(newStatus: string) {
+    this.newAccount.status =
+      newStatus.substring(0, 1).toUpperCase() + newStatus.substring(1);
+    this.logger.logStatusChange(`Account status changed to ${newStatus}`);
+  }
 }
