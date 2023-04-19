3ac74c81b24292bf9dabddaf8066b4dc43e62c84
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-19 14:01:08 -0300

    add mock content and styling to account component

--- src/app/components/new-account/new-account.component.ts
+++ src/app/components/new-account/new-account.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
 })
 export class NewAccountComponent {
-
+  accountStatus: string[] = ['Active', 'Inactive', 'Pending'];
 }
