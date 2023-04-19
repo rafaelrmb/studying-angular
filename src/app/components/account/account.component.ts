184d518a746361768359ea59bdb33daa5d8ed4f4
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-19 14:39:42 -0300

    add styling reactive to account status changes

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostBinding } from '@angular/core';
 import { LoggingService } from 'src/app/services/logging.service';
 
 @Component({
@@ -8,6 +8,7 @@ import { LoggingService } from 'src/app/services/logging.service';
 })
 export class AccountComponent {
   @Input('newAccount') newAccount!: { name: string; status: string };
+  @HostBinding('class') cssClass: string = 'bg-green-200';
 
   constructor(private logger: LoggingService) {}
 
@@ -15,5 +16,13 @@ export class AccountComponent {
     this.newAccount.status =
       newStatus.substring(0, 1).toUpperCase() + newStatus.substring(1);
     this.logger.logStatusChange(`Account status changed to ${newStatus}`);
+
+    if (newStatus === 'active') {
+      this.cssClass = 'bg-green-200';
+    } else if (newStatus === 'pending') {
+      this.cssClass = 'bg-orange-200';
+    } else {
+      this.cssClass = 'bg-red-200';
+    }
   }
 }
