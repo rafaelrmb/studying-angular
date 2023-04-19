7d6e6dbd1a6983c187ed34c0affa556586eaaa86
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-19 17:08:39 -0300

    implement users service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { OperationsCounterService } from './operations-counter.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  constructor(private opCounter: OperationsCounterService) {}
 
-  constructor() { }
+  usersList = [
+    {
+      id: 1,
+      name: 'John',
+      active: true,
+    },
+    {
+      id: 2,
+      name: 'Mary',
+      active: false,
+    },
+    {
+      id: 3,
+      name: 'Peter',
+      active: true,
+    },
+    {
+      id: 4,
+      name: 'Jason',
+      active: false,
+    },
+  ];
+
+  changeUserStatus(id: number, newStatus: boolean) {
+    this.usersList[id - 1].active = newStatus;
+    this.opCounter.increment();
+    console.log(this.opCounter.getCounter());
+  }
 }
