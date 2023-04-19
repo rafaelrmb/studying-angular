9a56e4315121bce7b79afe9669d62a5e1476cd45
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-19 10:27:48 -0300

    add an improved version of highlight directive

---
+++ src/app/directives/improved-highlight.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ImprovedHighlightDirective } from './improved-highlight.directive';
+
+describe('ImprovedHighlightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ImprovedHighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/improved-highlight.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appImprovedHighlight]'
+})
+export class ImprovedHighlightDirective {
+
+  constructor() { }
+
+}
