747e06076d2fd16ecf6a3a1df8ae6807f6063606
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-19 17:08:24 -0300

    implement operations counter service

--- src/app/services/operations-counter.service.ts
+++ src/app/services/operations-counter.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OperationsCounterService {
+  constructor() {}
 
-  constructor() { }
+  public counter: number = 0;
+
+  public increment() {
+    this.counter++;
+  }
+
+  public getCounter() {
+    return this.counter;
+  }
 }
