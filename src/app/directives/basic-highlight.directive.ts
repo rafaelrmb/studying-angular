d6600f7a520a45a8f925b538ad0079c71375cb2c
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-19 10:27:05 -0300

    add a basic directive to style an element

---
+++ src/app/directives/basic-highlight.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, OnInit, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appBasicHighlight]',
+})
+export class BasicHighlightDirective implements OnInit {
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+    this.elementRef.nativeElement.style.color = 'white';
+    this.elementRef.nativeElement.style.padding = '1rem';
+    this.elementRef.nativeElement.style.marginBottom = '1rem';
+  }
+}
