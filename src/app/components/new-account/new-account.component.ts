3ddf82f035fef0604d3a582b65bb9e6d065db59c
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-19 14:22:25 -0300

    add functionality to create new account and log it

--- src/app/components/new-account/new-account.component.ts
+++ src/app/components/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { LoggingService } from 'src/app/services/logging.service';
 
 @Component({
   selector: 'app-new-account',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-account.component.css'],
 })
 export class NewAccountComponent {
-  accountStatus: string[] = ['Active', 'Inactive', 'Pending'];
+  @Output() createdAccount = new EventEmitter<{
+    name: string;
+    status: string;
+  }>();
+
+  statusTypes: string[] = ['Active', 'Inactive', 'Pending'];
+
+  constructor(private logger: LoggingService) {}
+
+  addAccount(accountName: string, accountStatus: string) {
+    this.createdAccount.emit({
+      name: accountName,
+      status: accountStatus,
+    });
+
+    this.logger.logStatusChange(
+      `The account ${accountName} was created. And is currently ${accountStatus}!`
+    );
+  }
 }
