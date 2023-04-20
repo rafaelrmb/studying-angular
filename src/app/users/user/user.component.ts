8cb108f2b78fde1059b2b179793acb4694637d5a
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-20 15:08:24 -0300

    feature: load user info based on the route params

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+  user: { id: number; name: string };
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-  }
+    const { id, name } = this.route.snapshot.params;
+    this.user = { id, name };
 
+    this.route.params.subscribe((params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    });
+  }
 }
