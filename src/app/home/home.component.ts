19e5370344e5872ff0065639945f1f01cae0b913
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-21 12:59:08 -0300

    add a custom observable and subscription

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  private subscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    const customObservale = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.subscription = customObservale.subscribe((data: any) =>
+      console.log(data)
+    );
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
