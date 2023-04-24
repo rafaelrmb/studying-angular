0eb4f3b6229d2677818fbf5c1fb3c7439273c7a9
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-24 16:33:27 -0300

    add validators to gender and regex to email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,16 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       username: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      gender: new FormControl('prefer not to say'),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ]),
+      gender: new FormControl('prefer not to say', Validators.required),
     });
   }
 
   submitForm() {
-    console.log(this.signupForm);
+    console.log(this.signupForm.value);
   }
 }
