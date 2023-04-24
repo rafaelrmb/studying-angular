c212f42d567705803c647bc6fbbfe1208d5f3c64
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-24 18:43:11 -0300

    add assignment solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,64 +8,137 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  genders = ['male', 'female', 'other', 'prefer not to say'];
-  signupForm: FormGroup;
-  blockedUsernames = ['chris', 'anna'];
+  isAssignmentOn: boolean = true;
+  /* ---------------- THEORY CONTENT ----------------------------------*/
+  // genders = ['male', 'female', 'other', 'prefer not to say'];
+  // signupForm: FormGroup;
+  // blockedUsernames = ['chris', 'anna'];
 
-  ngOnInit() {
-    this.signupForm = new FormGroup({
-      userData: new FormGroup({
-        username: new FormControl(null, [
+  // ngOnInit() {
+  //   this.signupForm = new FormGroup({
+  //     userData: new FormGroup({
+  //       username: new FormControl(null, [
+  //         Validators.required,
+  //         this.invalidUsername.bind(this),
+  //       ]),
+  //       email: new FormControl(
+  //         null,
+  //         [
+  //           Validators.required,
+  //           Validators.email,
+  //           Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+  //         ],
+  //         this.blockedEmails
+  //       ),
+  //     }),
+  //     gender: new FormControl('prefer not to say', Validators.required),
+  //     hobbies: new FormArray([]),
+  //   });
+  // }
+
+  // submitForm() {
+  //   console.log(this.signupForm);
+  // }
+
+  // addHobby() {
+  //   (<FormArray>this.signupForm.get('hobbies')).push(
+  //     new FormControl(null, Validators.required)
+  //   );
+  // }
+
+  // get controls() {
+  //   return (this.signupForm.get('hobbies') as FormArray).controls;
+  // }
+
+  // invalidUsername(control: FormControl): { [s: string]: boolean } {
+  //   if (this.blockedUsernames.indexOf(control.value) !== -1) {
+  //     return { usernameIsBlocked: true };
+  //   }
+  //   return null;
+  // }
+
+  // blockedEmails(control: FormControl): Promise<any> | Observable<any> {
+  //   const promise = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       if (control.value === 'a@email.com') {
+  //         resolve({ emailIsBlocked: true });
+  //       } else {
+  //         resolve(null);
+  //       }
+  //     }, 1500);
+  //   });
+
+  //   return promise;
+  // }
+  /* ---------------- THEORY ENDS ----------------------------------*/
+
+  /* ---------------- ASSIGNMENT CONTENT ----------------------------------*/
+  projectForm: FormGroup;
+  statusList = ['Pending', 'Stable', 'Critical', 'Finished'];
+  allowedManagers = ['Chris', 'Anna'];
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      aboutProject: new FormGroup({
+        projectName: new FormControl(null, Validators.required),
+        projectDescription: new FormControl(null, Validators.required),
+        projectStartDate: new FormControl(null, [
           Validators.required,
-          this.invalidUsername.bind(this),
+          this.isDateInFuture,
         ]),
-        email: new FormControl(
+        projectEndDate: new FormControl(null, this.isDateInFuture),
+        projectStatus: new FormControl(null, Validators.required),
+      }),
+
+      managerInfo: new FormGroup({
+        managerName: new FormControl(null, [
+          Validators.required,
+          this.isManagerAllowed.bind(this),
+        ]),
+        managerEmail: new FormControl(
           null,
           [
             Validators.required,
             Validators.email,
             Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
           ],
-          this.blockedEmails
+          this.checkManagerEmail
         ),
       }),
-      gender: new FormControl('prefer not to say', Validators.required),
-      hobbies: new FormArray([]),
     });
   }
 
-  submitForm() {
-    console.log(this.signupForm);
-  }
-
-  addHobby() {
-    (<FormArray>this.signupForm.get('hobbies')).push(
-      new FormControl(null, Validators.required)
-    );
-  }
-
-  get controls() {
-    return (this.signupForm.get('hobbies') as FormArray).controls;
-  }
-
-  invalidUsername(control: FormControl): { [s: string]: boolean } {
-    if (this.blockedUsernames.indexOf(control.value) !== -1) {
-      return { usernameIsBlocked: true };
+  isManagerAllowed(control: FormControl): { [s: string]: boolean } {
+    if (this.allowedManagers.indexOf(control.value) === -1) {
+      return { allowedManager: false };
     }
     return null;
   }
 
-  blockedEmails(control: FormControl): Promise<any> | Observable<any> {
+  checkManagerEmail(control: FormControl): Observable<any> | Promise<any> {
     const promise = new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (control.value === 'a@email.com') {
-          resolve({ emailIsBlocked: true });
-        } else {
+        if (
+          control.value === 'chris@email.com' ||
+          control.value === 'anna@email.com'
+        ) {
           resolve(null);
+        } else {
+          resolve({ allowedEmail: false });
         }
       }, 1500);
     });
-
     return promise;
+  }
+
+  isDateInFuture(control: FormControl): { [s: string]: boolean } {
+    if (new Date(control.value) < new Date()) {
+      return { invalidDate: true };
+    }
+    return null;
+  }
+
+  submitForm() {
+    console.log(this.projectForm);
   }
 }
