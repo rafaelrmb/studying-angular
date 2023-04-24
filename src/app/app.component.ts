a6c7b477f1ea8e09dceabae389ff6264b7d5edfc
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-24 11:52:57 -0300

    add ngModel and ngForm to access data in the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('myForm') myForm!: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(myForm: NgForm) {
+  //   const { username, email, secret } = myForm.form.value;
+  //   console.log(username, email, secret);
+  //   console.log(myForm);
+  // }
+
+  onSubmit(myForm: NgForm) {
+    console.log(myForm);
   }
 }
