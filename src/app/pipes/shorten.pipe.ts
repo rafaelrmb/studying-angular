651bcf7234d5b34ad31177b76508c3f219b0d093
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-25 16:36:47 -0300

    add shorten pipe

---
+++ src/app/pipes/shorten.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shorten',
+})
+export class ShortenPipe implements PipeTransform {
+  transform(value: string, limit: number): string {
+    return value.length > limit ? value.substr(0, limit) + '...' : value;
+  }
+}
