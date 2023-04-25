8c347377c9f3d5cb0df5b08b4becd4d703d9a9d4
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-25 17:13:20 -0300

    add reverse pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ShortenPipe } from './pipes/shorten.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
+import { ReversePipe } from './pipes/reverse.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ShortenPipe, FilterPipe],
+  declarations: [AppComponent, ShortenPipe, FilterPipe, ReversePipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
