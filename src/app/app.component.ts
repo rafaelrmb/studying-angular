37eb0c854309bebfc9a4014157449c374b1ab665
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-25 19:22:22 -0300

    add posts output to UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Post } from './models/post.model';
 
 @Component({
   selector: 'app-root',
@@ -7,22 +9,54 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
-    console.log(postData);
+    this.http
+      .post<{ postId: string }>(
+        'https://ng-complete-course-97a40-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get<{ [key: string]: Post }>(
+        'https://ng-complete-course-97a40-default-rtdb.firebaseio.com/posts.json'
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe((posts) => {
+        this.loadedPosts = posts;
+        console.log(this.loadedPosts);
+      });
   }
 }
